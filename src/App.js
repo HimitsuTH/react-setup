a09845eed4c4d9112e3224c23f85c8cbe8601a12
Chinnawich <hotlnw159@gmail.com>
2022-05-02 16:53:49 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "App.css";
+
+//Import Components
+
+
+const App = () => (
+  <div>
+   
+    <h1 className="wow">Hello React!!!!!</h1>
+    
+  </div>
+);
+
+export default App;
